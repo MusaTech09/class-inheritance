7927dd7069e84604c3799e1369a9083493ad84ff
sebastian <stmr.tay@gmail.com>
2024-04-25 20:15:54 -0400

    Added Teacher class as child of Person.

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,14 @@
 const Person = require('./person');
 
-// Your code here 
+// Your code here
+
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience){
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
