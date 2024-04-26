a3fcbb4498d57c3153c6ad40452c38effb0582e0
sebastian <stmr.tay@gmail.com>
2024-04-25 20:03:44 -0400

    Added Student as child class of Parent.

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,14 @@
 const Person = require('./person');
 
-// Your code here 
+// Your code here
+
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA){
+    super(firstName, lastName)
+    this.major = major
+    this.GPA = GPA
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
